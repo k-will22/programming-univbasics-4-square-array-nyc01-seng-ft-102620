866d6c04748e97584e7934f865bc4b1d8096c46d
Keith Williams <thestreetkw@hotmail.com>
2020-10-11 07:19:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   square_array = []
   while counter < array.length do 
-    square_array.push(array[counter] ** array[counter])
+    square_array.push(array[counter] ** [counter])
     counter += 1
     end 
     square_array
